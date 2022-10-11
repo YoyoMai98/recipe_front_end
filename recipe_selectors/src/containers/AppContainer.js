e5db41765f67afca52fca221dce0413c5798def8
Hasmeet <hasmeetkaur.hk@gmail.com>
2022-10-11 17:57:29 +0100

    added styling for home page

--- recipe_selectors/src/containers/AppContainer.js
+++ recipe_selectors/src/containers/AppContainer.js
@@ -1,23 +1,39 @@
 import { Link } from "react-router-dom";
+import "./AppContainer.css";
 
 const AppContainer = ({recipes}) => {
 
     const recipeHero = recipes.filter(recipe => recipe.averageRating > 4);
 
+    const mainRecipe = recipeHero[1]
+
+    const rightRecipes = recipeHero.filter((recipe, index) => index !== 1 && index !== recipeHero.length-1)
+
     return (
-        <>
-            {recipeHero.map ((recipe, index) => {
-                return(
-                    <div className="recipe-hero" key={index}>
+        <div className="home">
+            {recipes.length >= 1 ? 
+            <>
+            <div className="up-recipes">
+                <div className="recipe-hero main-recipe-hero">
+                            <img src={mainRecipe.img} alt={mainRecipe.name} />
+                            <h4>{mainRecipe.name}</h4>
+                </div>
+                <div className="recipe-hero right_sider_recipes">
+                    {rightRecipes.map (recipe => {
+                    return(
                         <Link to={`/recipes/${recipe.id}`}>
+                        <div className="recipe-hero" key={recipe.id}>
+                            <img src={recipe.img} alt={recipe.name} />
                             <h4>{recipe.name}</h4>
+                        </div>
                         </Link>
-                        <img src={recipe.img} alt={recipe.name} />
-                    </div>
-                )
-            })
-            }
-        </>
+                    )
+                    })}
+                </div>
+            </div>
+            </>
+           : <div></div> }
+        </div>
     )
 }
 
